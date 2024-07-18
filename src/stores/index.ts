1c7c48f364f6120e775d1c885c3aa2dc0c708133
Yvo Niedrich <yvo.niedrich@gmail.com>
2024-07-18 20:08:36 +0200

    begin updating base-path

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,22 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
+const basePath = import.meta.env.BASE_URL;
+const indexPath = basePath + '/searchindex.json';
+
 export const useIndexStore = defineStore('index', () => {
     const index = ref<SearchIndex>({});
     
-    fetch('/searchindex.json')
+    fetch(indexPath)
         .then(rsp => rsp.json())
+        .then(json => {
+            if (!basePath) return json;
+            const obj: any = {};
+            Object.keys(json).forEach(path => {
+                obj[basePath + path] = json[path];
+            })
+            return obj;
+        })
         .then(json => index.value = json);
 
     return { index };
--- src/stores/registry.ts
+++ src/stores/registry.ts
@@ -1,17 +1,23 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+const basePath = import.meta.env.BASE_URL;
+const registryPath = basePath + '/registry.json';
+
 export const useRegistryStore = defineStore('registry', () => {
     const registry = ref<RecipeRegistryRecord[]>([]);
-    
-    fetch('/registry.json')
+
+    fetch(registryPath)
         .then(rsp => rsp.json())
         .then(rsp => {
             registry.value = rsp.map((record: any) => {
+                if (basePath) {
+                    record.path = record.path && basePath + record.path;
+                    record.preview = record.preview && basePath + record.preview;
+                }
                 record.modified = new Date(record.modified);
                 return record;
-            })
-            console.log('fetch done', rsp);
+            });
         });
 
     const categories = computed(() => {
